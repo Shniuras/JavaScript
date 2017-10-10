5ee73cd3581ee62e6e78be9cf06754c8f67d54b8
Shniuras <seimurinho@gmail.com>
2017-10-10 14:53:46 +0300

    Antradienis

---
+++ Antradienis/Pvz/script.js
@@ -0,0 +1,13 @@
+masyvas = [2,5,8, true];
+masyvas =[5];
+
+car = {
+	"make" : "audi";
+	"model" : "a6";
+	"drive" : function() {
+		console.log("driving..");
+	}
+};
+
+car.model //Bus iskviestas modelis
+car.drive() //Bus iskviesta funkcija
