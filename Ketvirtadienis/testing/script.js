7f0f9a279874e043fc81915a75ba955a2f6580ea
Shniuras <seimurinho@gmail.com>
2017-10-12 15:02:01 +0300

    Ketvirtadienis

---
+++ Ketvirtadienis/testing/script.js
@@ -0,0 +1,5 @@
+var random = (Math.ceil(Math.random() * 200 + Math.random() * 10));
+var random2 = (Math.ceil(Math.random() * 199 + Math.random() * 10));
+var random3 = (Math.ceil(Math.random() * 198 + Math.random() * 10));
+var random4 = (Math.ceil(Math.random() * 197 + Math.random() * 10));
+console.log('rgba'+'(',random, random2, random3, random4, ')');
