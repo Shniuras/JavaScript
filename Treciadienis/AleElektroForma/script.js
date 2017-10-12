d348a78a34472c78fe7b3537cfe694f05ff3cc7a
Shniuras <seimurinho@gmail.com>
2017-10-12 10:52:05 +0300

    Ketvirtadienis

--- Treciadienis/AleElektroForma/script.js
+++ Treciadienis/AleElektroForma/script.js
@@ -1,20 +1,61 @@
 var mpfa = document.getElementById('mpfa')
 var mpi = document.getElementById('mpi')
-var div_fizinis_asmuo = document.getElementById('fizinis_asmuo')
 var div_juridinis_asmuo = document.getElementById('juridinis_asmuo')
+var imo_kod = document.getElementById('ik');
+var imo_kod2= document.getElementById('ik2');
+
+mpi.addEventListener("click", function(){
+	if("click"){
+		mpi.style.backgroundColor = "red";
+		mpi.style.color = "white";
+		mpfa.style.backgroundColor = "white";
+		mpfa.style.color = "black";
+	}
+});
 
 mpfa.addEventListener("click", function(){
-	if(div_fizinis_asmuo.style.display === "none"){
-		div_fizinis_asmuo.style.display = "block";
-	} else {
-		div_fizinis_asmuo.style.display = "none";
+	if("click"){
+		mpfa.style.backgroundColor = "red";
+		mpfa.style.color = "white";
+		mpi.style.backgroundColor = "white";
+		mpi.style.color = "black";
+
 	}
 });
 
 mpi.addEventListener("click", function(){
-	if(div_juridinis_asmuo.style.display == "none"){
+	if(div_juridinis_asmuo.style.display = "none"){
 		div_juridinis_asmuo.style.display = "block";
-	} else {
+	}
+});
+
+mpfa.addEventListener("click", function(){
+	if(div_juridinis_asmuo.style.display == "block"){
 		div_juridinis_asmuo.style.display = "none";
 	}
 });
+
+function addTodo() {
+	var imkod = document.getElementById('imkod');
+	var pimkod = document.getElementById('pimkod')
+
+	if(imkod.value !== pimkod.value){
+		pimkod.style.backgroundColor = "red";
+		pimkod.style.color = "white";
+	} else {
+		pimkod.style.background = "none";
+		pimkod.style.color = "black";
+	}
+}
+
+function checkEmail() {
+
+    var email = document.getElementById('email');
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    if (!filter.test(email.value)) {
+    email.style.backgroundColor = "red";
+    email.style.color = "white";
+    return false;
+ 	}
+ }
